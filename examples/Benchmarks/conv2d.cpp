6eaad6b7db0a0eb419476cc98c74cf1920fa403c
Oswin Krause <oswin.krause@di.ku.dk>
2017-06-26 15:23:41 +0200

    added support for multiple images in conv2D

--- examples/Benchmarks/conv2d.cpp
+++ examples/Benchmarks/conv2d.cpp
@@ -6,7 +6,7 @@ using namespace remora;
 
 template<class E1, class E2>
 void benchmark(
-	vector_expression<E1, cpu_tag> const& image,
+	matrix_expression<E1, cpu_tag> const& image,
 	vector_expression<E2, cpu_tag> const& filter,
 	std::size_t num_channels,
 	std::size_t num_filters,
@@ -18,27 +18,24 @@ void benchmark(
 	std::size_t output_size2 = image_size2 - filter_size +1;
 	typedef typename E1::value_type value_type;
 
-	remora::vector<value_type> out(output_size1 * num_filters * output_size2, 0.0);
+	remora::matrix<value_type> out(image().size1(), output_size1 * num_filters * output_size2, 0.0);
 	double minOptTime = std::numeric_limits<double>::max();
-	for(std::size_t i = 0; i != 20; ++i){
+	for(std::size_t i = 0; i != std::max<std::size_t>(1, std::size_t(20/image().size1())); ++i){
 		Timer time;
 		kernels::conv2d(image,filter,out, num_channels, num_filters, image_size1, image_size2, filter_size, filter_size,0,0);
 		minOptTime = std::min(minOptTime,time.stop());
 	}
 
 	double mults = output_size1 * output_size2 * filter_size * filter_size * num_filters * num_channels;
-	double flops = mults /1024/1024/minOptTime;
+	double flops = image().size1() * mults /1024/1024/minOptTime;
 
 	std::cout<<output_size1<<"\t"<<filter_size<<"\t"<<num_channels<<"\t"<< num_filters<<"\t";
 	std::cout<<"\t"<<flops<< std::endl;
 }
 
-
-int main(int argc, char **argv) {
+template<class T>
+void benchmark(std::size_t num_channels, std::size_t num_outputs, std::size_t num_images){
 	std::cout<<"Flops"<<std::endl;
-	std::size_t num_channels = 8;
-	std::size_t num_outputs = 32;
-	std::cout<<"performance float"<<std::endl;
 	for(std::size_t filterSize = 4; filterSize != 32; filterSize *= 2){
 		for(std::size_t iter = 0; iter != 5; ++iter){
 			std::size_t sizeOut1 = 3+16 * (2<<iter);
@@ -46,12 +43,14 @@ int main(int argc, char **argv) {
 			std::size_t sizeIm1 = sizeOut1 + filterSize-1;
 			std::size_t sizeIm2 = sizeOut2 + filterSize-1;
 
-			remora::vector<float> image(num_channels * sizeIm1 * sizeIm2);
-			remora::vector<float> filter(num_channels * num_outputs *  filterSize * filterSize);
-
-			for(std::size_t i = 0; i != num_channels * sizeIm1; ++i){
-				for(std::size_t j = 0; j != sizeIm2; ++j){
-					image(i * sizeIm2 + j)  = 1.0/(num_channels * sizeOut1)*i + 0.1 - (0.1/sizeOut2)*j;
+			remora::matrix<T> image(num_images, num_channels * sizeIm1 * sizeIm2);
+			remora::vector<T> filter(num_channels * num_outputs *  filterSize * filterSize);
+			
+			for(std::size_t im = 0; im != num_images; ++im){
+				for(std::size_t i = 0; i != num_channels * sizeIm1; ++i){
+					for(std::size_t j = 0; j != sizeIm2; ++j){
+						image(im, i * sizeIm2 + j)  = 1.0/(num_channels * sizeOut1)*i + 0.1 - (0.1/sizeOut2)*j;
+					}
 				}
 			}
 			for(std::size_t i = 0; i != num_channels * num_outputs * filterSize; ++i){
@@ -63,28 +62,12 @@ int main(int argc, char **argv) {
 			benchmark(image,filter,num_channels,num_outputs, sizeIm1, sizeIm2, filterSize);
 		}
 	}
+}
+
+
+int main(int argc, char **argv) {
+	std::cout<<"performance float"<<std::endl;
+	benchmark<float>(8,32,16);
 	std::cout<<"performance double"<<std::endl;
-	for(std::size_t filterSize = 4; filterSize != 32; filterSize *= 2){
-		for(std::size_t iter = 0; iter != 6; ++iter){
-			std::size_t sizeOut1 = (3+16 * 2<<iter);
-			std::size_t sizeOut2 = (3+16 * 2<<iter);
-			std::size_t sizeIm1 = sizeOut1 + filterSize-1;
-			std::size_t sizeIm2 = sizeOut2 + filterSize-1;
-
-			remora::vector<double> image(num_channels * sizeIm1 * sizeIm2);
-			remora::vector<double> filter(num_channels * num_outputs *  filterSize * filterSize);
-			for(std::size_t i = 0; i != num_channels * sizeIm1; ++i){
-				for(std::size_t j = 0; j != sizeIm2; ++j){
-					image(i * sizeIm2 + j)  = 1.0/(num_channels * sizeOut1)*i + 0.1 - (0.1/sizeOut2)*j;
-				}
-			}
-			for(std::size_t i = 0; i != num_channels * num_outputs * filterSize; ++i){
-				for(std::size_t j = 0; j != filterSize; ++j){
-					filter(i * filterSize + j)  = 1.0/(num_channels * filterSize)*i + 0.1 - (0.1/filterSize)*j;
-				}
-			}
-
-			benchmark(image,filter,num_channels,num_outputs, sizeIm1, sizeIm2, filterSize);
-		}
-	}
+	benchmark<double>(8,32,16);
 }
